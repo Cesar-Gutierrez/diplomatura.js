a96ceb1ff7b1baa9edfd970159451fbc66582af3
MCele <celeste.ramos@fi.uncoma.edu.ar>
2020-07-16 00:13:14 -0300

    get alumnos

--- grupos/grupo9/005-ejercicios/src/controllers/alumnos.js
+++ grupos/grupo9/005-ejercicios/src/controllers/alumnos.js
@@ -1,12 +1,32 @@
 import express from 'express';
-
+import * as connectDB from '../utils/crud-mongo';
 const router = express.Router();
 
-router.get('/', function (req, res) {
+const dbName = 'diplomaturajs';
+
+router.get('/', async function (req, res) {
   // Completar
-  
-  res.json({});
+  const params = req.query;
+  console.log(params);
+  let resp = await getAlumnos('alumno', params);
+  console.log("res: ", resp);
+  //connectDB.disconnect(conec)
+  res.json(resp);
 });
+
+
+const getAlumnos = async (collection, params) => {
+  // abrimos la conexión
+  let client = await connectDB.connect();
+  const db = client.db(dbName);
+  // hacemos la búsqueda en la colección alumno
+  const res = connectDB.find(db, collection, params);
+  client.close();
+  return res;
+}
+
+
+
 
 router.get('/:id', function (req, res) {
   // Completar
