85590aa2654163ba584082f318802636d255fc2e
MCele <celeste.ramos@fi.uncoma.edu.ar>
2020-07-18 03:19:31 -0300

    update en mongo-cru

--- grupos/grupo9/005-ejercicios/src/controllers/alumnos.js
+++ grupos/grupo9/005-ejercicios/src/controllers/alumnos.js
@@ -2,15 +2,14 @@ import express from 'express';
 import * as connectDB from '../utils/crud-mongo';
 const router = express.Router();
 
-const dbName = 'diplomatura';
-const collectionName = 'alumnos';
+const dbName = 'diplomaturajs';
+const collectionName = 'alumno';
 const singleName = 'Alumno';
 
 router.get('/', async function (req, res) {
   const params = req.query;
   res.json(await getAlumnos(collectionName, params));
 });
-
 
 const getAlumnos = async (collection, params) => {
   // abrimos la conexión
@@ -27,8 +26,14 @@ router.get('/:id', async function (req, res) {
   let resp = {};
   if (id) {
     resp = await getAlumnoById(collectionName, id);
+    if (resp) {
+      res.json(resp);
+    }
+    else {
+      res.status(404).send({ mensaje: "Elemento no encontrado" })
+    }
   }
-  res.json(resp);
+
 });
 
 const getAlumnoById = async (collection, id) => {
@@ -46,9 +51,6 @@ const getAlumnoById = async (collection, id) => {
 }
 
 router.post('/', async function (req, res) {
-  // TIP: En req.body viene los datos
-/*   console.log("Esto tiene el body :", req.body);
-  console.log("Esto tiene el query :", req.query); */
   let resp;
   let message;
   //let message;return;
@@ -74,6 +76,73 @@ const insertarAlumno = async (collection, params) => {
 }
 
 
+router.delete('/:id', async function (req, res) {
+  const id = req.params.id;
+  let resp;
+  if (req.params.length === 0 || !id) {
+    res.status(404).send("Debe ingresar un id.");
+  }
+  else {
+    resp = await deleteAlumnoByID(collectionName, id);
+
+    res.status(resp.codigo).send(resp.mensaje); //  salidaDel(resp, res);
+  }
+});
+
+// function salidaDel(respuesta, res) {
+//   res.status(respuesta.codigo).send(respuesta.mensaje);
+// }
+
+const deleteAlumnoByID = async (collection, id) => {
+  // abrimos la conexión 
+  let client = await connectDB.connect();
+  const db = client.db(dbName);
+  const resp = await connectDB.deleteByID(db, collection, id);
+  client.close();
+  return resp;
+  // try {
+  //   const resp = await connectDB.deleteByID(db, collection, id);
+  //   //const salida = salir(resp, client);// console.log("respuesta delete",resp);
+  //   client.close();
+  //   return resp;
+  // }
+  // catch (err) {
+  //   client.close();
+  //   return { codigo: 500, mensaje: "Error de eliminación." };
+  // }
+}
+
+// async function salir(respuesta, client) {
+//   // console.log("salir: ",respuesta);
+//   client.close();
+//   return respuesta;
+
+// }
+router.put('/:id', async function (req, res) {
+  const id = req.params.id;
+  let resp = {};
+  if (id && req.body && Object.keys(req.body).length !== 0) {
+    resp = await updateAlumno(collectionName, id, req.body);
+    if (resp.codigo === 200) {
+      res.status(resp.codigo).send(resp.result);
+    }
+    else {
+      res.status(resp.codigo).send(resp.mensaje);
+    }
+  } else {
+    res.status(400).send({ mensaje: "No se actualizado el elemento: no se recibieron datos" })
+  }
+});
+
+const updateAlumno = async (collection, id, alumno) => {
+  // abrimos la conexión
+  let client = await connectDB.connect();
+  const db = client.db(dbName);
+  // hacemos la búsqueda en la colección alumno
+  const res = await connectDB.updateDoc(db, collection, id, alumno);
+  client.close();
+  return res;
+}
 
 // Completar el resto de los métodos
 // router....
--- grupos/grupo9/005-ejercicios/src/utils/crud-mongo.js
+++ grupos/grupo9/005-ejercicios/src/utils/crud-mongo.js
@@ -2,7 +2,7 @@ import { MongoClient, ObjectId } from 'mongodb';
 
 // Connection URL
 const urlDefault = 'mongodb://localhost:27017';
-
+//const urlDefault = process.env.BASEDATOS || 'mongodb://localhost:27017';
 
 // Create a new MongoClient
 export async function connect(url = urlDefault) {
@@ -43,6 +43,7 @@ export async function findById(db, nameCollection, id) {
         return await collection.findOne({ _id: idObj });
     } catch (error) {
         console.log("No fue posible realizar la consulta! Error:  ", error);
+        return;
     }
 }
 
@@ -59,3 +60,69 @@ export async function insertarDoc(db, nameCollection, doc) {
         console.log("No fue posible realizar la insercion! Error:  ", error);
     }
 }
+
+export async function updateDoc(db, nameCollection, id, doc) {
+    if (!db || !nameCollection || !id || !doc) {
+        console.log("BD o elemento incorrecto");
+        return;
+    }
+    let elem = await findById(db, nameCollection, id);
+    if (!elem) {
+
+        return { codigo: 400, mensaje: "Elemento no encontrado" }
+    }
+    try {
+        const collection = db.collection(nameCollection);
+
+        const idObj = new ObjectId(id);
+        const result = await collection.findOneAndUpdate({ _id: idObj }, { $set: doc }, { returnOriginal: false });
+        if (result.lastErrorObject.n === 1 && result.lastErrorObject.updatedExisting) {
+            return { codigo: 200, mensaje: "Elemento actualizado con éxito", result: result.value }
+        }
+        else {
+            return { codigo: 500, mensaje: "No fue posible realizar la actualización" }
+        }
+
+    } catch (error) {
+        return { codigo: 500, mensaje: "Error: No fue posible realizar la actualización" }
+    }
+}
+
+export async function deleteByID(db, nameCollection, id) {
+    if (!db || !nameCollection || !id) {
+        return { Codigo: 500, Mensaje: "BD, colección y/o id son incorrectos" };
+    }
+    const collection = await db.collection(nameCollection);
+    try {
+        const elem = await findById(db, nameCollection, id);
+        if (!elem) {
+            return { codigo: 404, mensaje: "No existe el elemento que intenta eliminar" };
+        }
+    }
+    catch (err) {
+        return { Codigo: 400, Mensaje: "Error interno al intentar recuperar el elemento a eliminar" };
+    }
+    try {
+        const respDel = await collection.deleteOne({ "_id": ObjectId(id) });
+
+        return (respDel.deletedCount === 1) ?
+            { codigo: 200, mensaje: "Eliminación exitosa." } :
+            { codigo: 500, mensaje: "Error mongo interno en el proceso de eliminación." };
+    } catch (error) {
+        return { codigo: 500, mensaje: "Error no se pudo eliminar el elemento." };
+    }
+}
+
+// async function respuestaDelete(result) {
+//     // console.log(result);
+//     if (result.deletedCount === 1) {
+//         //    console.log("despues de eliminar",result);
+//         // console.log("Eliminación exitosa.");
+//         return { codigo: 200, mensaje: "Eliminación exitosa." };
+//     }
+//     else {
+//         // console.log("Error base:No fue posible eliminar el elemento! Error:  ");
+//         return { codigo: 500, mensaje: "Error mongo interno en el proceso de eliminación." };
+//     }
+
+// }
