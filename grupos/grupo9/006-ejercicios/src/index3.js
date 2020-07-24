d752ff037239d3c68ca122ecf9389ffe2e5f7dec
NanoWi <mariano.wilson@est.fi.uncoma.edu.ar>
2020-07-23 23:36:39 -0300

    resolucion_ parte eje3

--- grupos/grupo9/006-ejercicios/src/index3.js
+++ grupos/grupo9/006-ejercicios/src/index3.js
@@ -1,13 +1,12 @@
- let TODO = ['Queso', 'Tomates', 'Zanahorias', 'Toalla'];
- 
 
  const generarLista=()=>{
+    
     let lista= `<ul>
     `
     TODO.forEach(elemento => {
    
    lista=lista+    `
-        <li>${elemento}</li>
+        <li>${elemento}<button id ="botonDelete${elemento}">eliminar</button> </li>
       `
            
     });
@@ -29,17 +28,41 @@ const cargarItem=(valorItem)=>{
     return result;
     }
  
- refrescar();   
-document.getElementById("botonAgregar").onclick=generarItemHtml;
 
 function refrescar(){
+    
     document.getElementById('app').innerHTML = generarLista();
     document.getElementById("agregarItem").value='';
+    TODO.forEach(element => {
+        document.getElementById("botonDelete"+element).onclick=borrarItem;    
+    });
 }
 
 function generarItemHtml(){
-        if (cargarItem(document.getElementById("agregarItem").value))
+        if (cargarItem(document.getElementById("agregarItem").value)){
+        localStorage.setItem("todo",TODO);
+        
         refrescar();
+        }
 
 }
 
+function borrarItem(e){
+    console.log('quiero borrar: ',e.target.previousSibling.data );
+    
+    TODO.splice(TODO.indexOf(e.target.previousSibling.data),1);
+    localStorage.setItem("todo",TODO);
+    refrescar();
+}
+
+
+let TODO= ['Queso', 'Tomates', 'Zanahorias', 'Toalla'];
+
+ if (Object.keys(localStorage).find(e=>e==='todo')){
+   
+    TODO = localStorage.getItem('todo');
+    TODO=(TODO)?TODO.split(','):[];
+}
+ 
+refrescar();   
+document.getElementById("botonAgregar").onclick=generarItemHtml;
