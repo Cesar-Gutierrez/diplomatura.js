b8a4475f8085a4747dffad509df6ece887e33aa2
NanoWi <mariano.wilson@est.fi.uncoma.edu.ar>
2020-07-25 01:33:24 -0300

    ejercicio 4 globos

--- grupos/grupo9/006-ejercicios/src/index4.js
+++ grupos/grupo9/006-ejercicios/src/index4.js
@@ -1,31 +1,60 @@
-`<div id="globoVerde"></div>
-    <div id="globoRojo"></div>
-<div id="globoAzul"></div>`;
-
 //obtenemos un número aleatorio entre 10 y 30
 let contGlobos = Math.floor(Math.random() * (30 - 10) + 10);
 console.log(contGlobos);
 const generarListaGlobos = () => {
-  let listaGlobos;
+  let listaGlobos='';
   let colores = ['Azul', 'Rojo', 'Amarillo'];
   for (let i = 0; i < contGlobos; i++) {
     let pos = i % colores.length; // obtenemos el resto de dividir por 3
+    if (i%10===0)
+      listaGlobos+='<br>';
+    
     listaGlobos += `<div class="globo${colores[pos]}" id="globo${i}"></div>`;
   }
-  console.log(listaGlobos);
-  //  lista = lista + `</ul>`;
   return listaGlobos;
 };
 
 document.getElementById('app').innerHTML = generarListaGlobos();
+let pos;
 for (let i = 0; i < contGlobos; i++) {
-  document.getElementById('botonDelete' + element).onclick = borrarItem;
+pos=i*100;
+  document.getElementById('globo' + i).onclick = borrarGlobo;
+ // document.getElementById('globo' + i).setAttribute("style", "margin-left:"+pos+"px;margin-top:"+10+"px;");
+
+  //document.getElementById('globo' + i).style.margin="("+pos+",15)";
 }
 
-TODO.forEach((element) => {
-  document.getElementById('globo' + i).onclick = borrarGlobo;
-});
 
 function borrarGlobo(event) {
-  console.log(event);
+  console.log(event,event.target.attributes.id.nodeValue);
+  if (eliminarElemento(event.target.attributes.id.nodeValue)){
+    contGlobos--;
+    if (contGlobos===0)
+    alert("Ganaste");
+  }
 }
+
+ 
+function eliminarElemento(id){
+	let element = document.getElementById(id);	
+	if (!element){
+		return false;
+	} else {
+		padre = element.parentNode;
+    padre.removeChild(element);
+    return true;
+	}
+} 
+
+// top es moverme dede arriba hacia abajo
+// left moverme desde izquierda a derecha
+
+/* console.log(document.getElementById("globo1").offsetTop);
+console.log(document.getElementById("globo2").offsetTop);
+console.log(document.getElementById("globo3").offsetTop);
+
+
+console.log(document.getElementById("globo1").offsetLeft);
+console.log(document.getElementById("globo2").offsetLeft);
+document.getElementById("globo3").setAttribute("style", "margin-left: 100px;");
+ */
